3a3c1658d57e6f44491aa4dcb619dc6bcbbd17cc
Guillaume Vilar <guillaume.vilar@arista.com>
2023-04-13 23:58:25 +0100

    [CHAP2]: test CI with test that fails

--- chap2/test_hello.py
+++ chap2/test_hello.py
@@ -13,3 +13,7 @@ def test_add():
     
     # Test case 4: Test with decimal numbers
     assert add(2.5, 3.5) == 6.0  # Expected result: 2.5 + 3.5 = 6.0
+
+def test_failing():
+    # Fail that should fail (for test CI)
+    assert add(1, 1) == 3
