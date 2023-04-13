5679ed34318d1140dc89262c95399e9a32a832bb
Guillaume Vilar <guillaume.vilar@arista.com>
2023-04-13 23:44:17 +0100

    [CHAP2]: Moving listening port to 80

--- chap2/gunicorn.conf.py
+++ chap2/gunicorn.conf.py
@@ -1,3 +1,3 @@
 workers = 4  # Number of worker processes
-bind = "0.0.0.0:8080"  # Address and port to bind to
+bind = "0.0.0.0:80"  # Address and port to bind to
 module = "hello:app"  # Flask application object
